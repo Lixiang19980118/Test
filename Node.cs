6d2258bda6a18a6386b7fa3bd6050d625e05f440
LiXiang <1339846326@qq.com>
2024-05-14 20:31:25 +0800

    添加项目文件。

---
+++ Node.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public List<Node> Children { get; set; } = new List<Node>();
+
+
+        public List<Node> BuildTree(List<Node> list)
+        {
+            var result = new List<Node>();
+            if (list == null || list.Count == 0)
+            {
+                return list;
+            }
+
+            Queue<Node> queue = new Queue<Node>(list);
+            while (queue.Count > 0)
+            {
+                Node node = queue.Dequeue();
+                if (node.ParentId != "")//非根节点
+                {
+                    var parent = list.First(t => t.Id == node.ParentId);
+                    parent.Children.Add(node);
+                }
+                else//根节点
+                {
+                    result.Add(node);
+                }
+            }
+            return result;
+        }
+
+        public List<Node> DebuildTree(List<Node> tree)
+        {
+            var result = new List<Node>();
+            if (tree==null || !tree.Any())
+            {
+                return result;
+            }
+            Stack<Node> stack = new Stack<Node>(tree);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node.Children.Any())
+                {
+                    foreach (var item in node.Children)
+                    {
+                        stack.Push(item);
+                    }
+                }
+                node.Children = new List<Node>();
+                result.Add(node);
+            }
+            return result;
+        }
+    }
+
+
+    
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test;
+
+Console.WriteLine("Hello, World!");
+
+
+List<Node> nodes = new List<Node>();
+nodes.Add(new Node()
+{
+    Id = "001",
+    ParentId=""
+});
+nodes.Add(new Node()
+{
+    Id = "002",
+    ParentId = ""
+});
+nodes.Add(new Node()
+{
+    Id = "001-1",
+    ParentId = "001"
+});
+nodes.Add(new Node()
+{
+    Id = "001-2",
+    ParentId = "001"
+});
+nodes.Add(new Node()
+{
+    Id = "001-1-1",
+    ParentId = "001-1"
+});
+nodes.Add(new Node()
+{
+    Id = "001-1-2",
+    ParentId = "001-1"
+});
+
+nodes.Add(new Node()
+{
+    Id = "002-1",
+    ParentId = "002"
+});
+nodes.Add(new Node()
+{
+    Id = "002-2",
+    ParentId = "002"
+});
+
+
+var node = new Node();
+var temp = node.BuildTree(nodes);
+var dNodes = node.DebuildTree(temp);
+Console.WriteLine("============");
